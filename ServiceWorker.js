61634f73a570546e66b220b48100a92b5bf44284
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2024-06-21 13:44:16 +0300

    Test build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany121212-ChessTG1212-3";
 const contentToCache = [
-    "Build/c598b1bb4ff0cabbff7b366ffc67a239.loader.js",
-    "Build/460315dbb1e582a708722d8cdd619e98.framework.js",
-    "Build/698f2b42afb444a5f2c84c7aa452bf74.data",
-    "Build/940dd6b99547ba8e786455b7265151ba.wasm",
+    "Build/8d29fbd3ce8e2bacd0b9a73c1ab389a6.loader.js",
+    "Build/4f6e9972efca9ee9a325b6ae366c1377.framework.js",
+    "Build/5af8a8841b63c3b9f7d77163436e19a2.data",
+    "Build/a72fecaed1ed0de663c0b1050582d577.wasm",
     "TemplateData/style.css"
 
 ];
@@ -11,11 +11,11 @@ const contentToCache = [
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
+    //e.waitUntil((async function () {
+      //const cache = await caches.open(cacheName);
+      //console.log('[Service Worker] Caching all: app shell and content');
+      //await cache.addAll(contentToCache);
+    //})());
 });
 
 self.addEventListener('fetch', function (e) {
