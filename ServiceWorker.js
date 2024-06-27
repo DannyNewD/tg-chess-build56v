eaf7fb367d4d9f239b4f981df0189dcb9b610d95
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2024-06-27 11:55:59 +0300

    Build3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany121212-ChessTG1212-3";
 const contentToCache = [
-    "Build/fb29ef148f29f6b5263af2c9684494e5.loader.js",
+    "Build/13de8e7dc25ae1dea4b2de32828f1104.loader.js",
     "Build/62e0b3ce52e4d349e0178dca4067d183.framework.js",
-    "Build/f3668fa5d91ebcc67019a22dd51f8c8a.data",
-    "Build/ce55edc5f743fa5438929d5c20a3002a.wasm",
+    "Build/024641d5b8f729cf957273f0f01f99f5.data",
+    "Build/e70e983897ee8faa2b640639b98a52f6.wasm",
     "TemplateData/style.css"
 
 ];
